268ab3d1ac2f7ea69d4f4296ecce4aebe2b78bb1
Greg <gregorykimanthi100@gmail.com>
2023-11-23 23:54:09 +0300

    added functionality to the items left feature

--- public/script/completedTask.js
+++ public/script/completedTask.js
@@ -12,3 +12,37 @@ function completedTodo(todo){
     document.querySelector(".round p").classList.add("line")
     .catch(error=>console.error(error));
 }
+const checkboxes = document.querySelectorAll('.checkbox');
+const check =document.querySelector(".checkbox");
+let completedTodosCount = 0; // Counter for completed todos
+let tod =check.getAttribute("onchange")
+
+const newTodos=[tod];
+
+    // const todoArray = getTodos(todos); 
+    // console.log(todoArray);
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+    // Update counter based on checkbox state
+    if (this.checked) {
+      completedTodosCount++;
+    } else {
+      completedTodosCount--;
+    }
+
+    // Update the UI with the new counter value
+    updateCounterUI();
+  });
+});
+const updatedTodos =newTodos.map(todo=>todo.split(","));
+const allTodos = updatedTodos[0].length;
+
+function updateCounterUI() {
+  // You can update the UI with the new completedTodosCount value
+  // For example, if you have an element with id 'counter', you can do:
+  document.getElementById('complete').innerText = allTodos-completedTodosCount +" items left";
+}
+
+
+// You can also call updateCounterUI initially to set the counter value
+updateCounterUI();
